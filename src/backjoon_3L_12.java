23e25c62037bf3acb1ff0300318f915839ee2db8
GrowMyself <mirea70@naver.com>
2022-06-16 02:10:40 +0900

    3L_12 succeded

---
+++ src/backjoon_3L_12.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class backjoon_3L_12 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int A = 1;
+        int B = 1;
+
+        while(true){
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+            if(A == 0 && B == 0) {
+                break;
+            }
+            bw.write((A+B)+"\n");
+        }
+        bw.close();
+    }
+}

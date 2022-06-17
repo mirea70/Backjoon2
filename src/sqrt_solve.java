25281ca87b8fabeddad3a22279a0b0ffb4146874
GrowMyself <mirea70@naver.com>
2022-06-17 12:22:25 +0900

    sqrt_solve succeded

---
+++ src/sqrt_solve.java
@@ -0,0 +1,10 @@
+public class sqrt_solve {
+    public static void main(String[] args) {
+        System.out.println(String.format("%.2f" ,sqrt(6,144)));
+    }
+    private static double sqrt(int n, int num) {
+        if(n == 0) return 1;
+        double result = ((sqrt(n -1,  num)) * (sqrt( n -1,  num)) + num ) / (double)(2 * sqrt( n -1,  num));
+        return result ;
+    }
+}

cf9da0c4decca8d4f54e2dacf535aaa4dda13651
GrowMyself <deae670@daum.net>
2022-07-12 02:52:28 +0900

    baekjoon 2q solved~

--- src/New_Id2.java
+++ src/New_Id2.java
@@ -5,7 +5,7 @@ public class New_Id2 {
     public static void main(String[] args) {
         solve sol = new solve();
 
-        String new_id = "abcdefghijklmn...p";
+        String new_id = "a......b.+*";
         System.out.println(sol.solution(new_id));
     }
 }
@@ -21,7 +21,6 @@ class solve {
             if (Character.isUpperCase(c)) chList.set(chList.indexOf(c), Character.toLowerCase(c));
         }
 
-
         // 2단계 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
         Queue q = new LinkedList();
         q.add(' ');
@@ -35,22 +34,31 @@ class solve {
             }
 
         // 3단계 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
-        q.add('.');
-        while (!q.isEmpty()) {
-            q.poll();
-
-            for (int i = 1; i < chList.size(); i++) {
-                if ((chList.get(i) == '.' && chList.get(i-1) == '.')) q.add(chList.get(i));
-            }
-
-            chList.remove(q.poll());
+//        q.add('.');
+        for (int i = 1; i < chList.size(); i++) {
+            if ((chList.get(i) == '.' && chList.get(i-1) == '.')) q.add(chList.get(i));
         }
+
+        while (!q.isEmpty()) {
+            chList.remove(chList.indexOf(q.poll()));
+        }
+//        ArrayList<Integer> delete = new ArrayList<>();
+//        for (int i = 1; i < chList.size(); i++) {
+//            if ((chList.get(i) == '.' && chList.get(i-1) == '.')) delete.add(i);
+//        }
+//
+//        for(int i = delete.size() - 1; i >= 0; i--){
+//            chList.remove((int) delete.get(i));
+//        }
+
+        // . 다없어지는 거해결
 
         // 4단계 마침표(.)가 처음이나 끝에 위치한다면 제거
 
         if(chList.get(0) == '.') chList.remove(0);
 
         if(!chList.isEmpty() && chList.get(chList.size()-1) == '.') chList.remove(chList.size()-1);
+        // --> while문으로 바꿔보기
 
         // 5단계 빈 문자열이라면, new_id에 "a"를 대입합니다.
         if(chList.size() == 0) chList.add('a');

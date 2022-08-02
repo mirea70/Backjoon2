b86ebe6441ef5011492a84f47e7b581b4c8ad496
mirea70 <deae670@daum.net>
2022-08-02 19:34:34 +0900

    ing

---
+++ src/AS/w10/rightBefore.java
@@ -0,0 +1,19 @@
+package AS.w10;
+
+public class rightBefore {
+}
+
+class rightBefore_Solution {
+    public String solution(String m, String[] musicinfos) {
+        // 조건 일치하는 제목, 재생시간이 담긴 배열을 담을 리스트 생성
+        // musicinfos 순회
+        // 요소를 , 기준으로 배열 생성
+        // 생성된 배열의 마지막 인덱스의 글자들이 인자 m의 있는 요소의 순서대로 존재하는지 체크
+        // 조건 만족시 리스트에 추가
+        // 순회 끝
+        // 리스트 사이즈 > 1 일 경우, 리스트의 각 요소(배열)의 2번째 인덱스(재생시간)이 가장 긴 요소의 1번째 인덱스(제목)을 반환
+        // 리스트 사이즈 0이면, "(None)" 반환
+        String answer = "";
+        return answer;
+    }
+}

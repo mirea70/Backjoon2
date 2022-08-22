9caba3b79cf70c5517663c1fae7764c3af0ffee4
GrowMyself <deae670@daum.net>
2022-08-23 02:30:44 +0900

    baekjoon lv6 solved

---
+++ src/baekJoon/baekjoon_6L_07.java
@@ -0,0 +1,17 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class baekjoon_6L_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder s1 = new StringBuilder(sc.next());
+        StringBuilder s2 = new StringBuilder(sc.next());
+
+        s1.reverse();
+        s2.reverse();
+
+        System.out.println(Math.max(Integer.parseInt(s1.toString()), Integer.parseInt(s2.toString())));
+    }
+}

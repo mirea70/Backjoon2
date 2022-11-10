19e2c43eb658641bed545706aa2f9b96eb1059dc
GrowMyself <deae670@daum.net>
2022-11-11 06:56:48 +0900

    baekjoon 203 - 8 Solve

---
+++ src/AS_Basic/q203_8.java
@@ -0,0 +1,20 @@
+package AS_Basic;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class q203_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] arr = sc.nextLine().split(" ");
+        int[] ns = new int[2];
+//        ns[0] = Integer.parseInt(arr[0] + arr[1]);
+//        ns[1] = Integer.parseInt(arr[2] + arr[3]);
+
+        BigInteger n1 = new BigInteger(arr[0] + arr[1]);
+        BigInteger n2 = new BigInteger(arr[2] + arr[3]);
+
+        System.out.print(n1.add(n2));
+    }
+}
